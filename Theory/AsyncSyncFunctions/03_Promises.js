ec1324a6bb855efbc480c81ee7c2eddb60132ad7
rakshitnayak <rdsonnn@gmail.com>
2024-01-13 23:58:27 +0530

    promise more info

--- Theory/AsyncSyncFunctions/03_Promises.js
+++ Theory/AsyncSyncFunctions/03_Promises.js
@@ -41,4 +41,34 @@ function onDone(data) {
 
 kiratsReadFile().then(onDone);
 
+
+Creating a promise:
+- A promise represents the eventual completion or failure of an asynchronous operation.
+- The Promise constructor takes a function with two parameters: resolve and reject
+
+const myPromise = new Promise((resolve, reject) => {
+  // Asynchronous operation goes here
+  // If successful, call resolve with the result
+  // If there's an error, call reject with the error
+});
+
+
+Resolving a Promise: Use the resolve function when the asynchronous operation is successful.
+
+
+const successfulPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('Operation succeeded!');
+  }, 1000);
+});
+
+Rejecting a Promise: Use the reject function when there's an error during the asynchronous operation.
+
+const failedPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject('Operation failed!');
+  }, 1000);
+});
+
+
  */
