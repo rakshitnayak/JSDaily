bf0c9c991207b8acd1f60678640f74c64dde3684
rakshitnayak <rdsonnn@gmail.com>
2024-01-21 00:49:34 +0530

    Add time complexity for For loops

---
+++ DSA/BigO/timeComplexity.js
@@ -0,0 +1,14 @@
+/**
+
+Time complexites for loops
+
+let i;
+for(i=0; i<n; i++) ---- O(n);
+for(i=0; i<n; i=i+2;) ---- n/2 so O(n);
+for(i=n; i>1; i--;) ---- O(n);
+for(i=1; i<n; i=i*2) ---O(log2n) // read log to the base 2 n
+for(i=1; i<n; i=i*3) ---O(log3n) // read log to the base 3 n
+for(i=n; i>1; i=i/2) ---O(log2n) // read log to the base 2 n
+
+
+**/
