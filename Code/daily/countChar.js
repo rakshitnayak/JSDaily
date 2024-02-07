588fcf5c1e67940d558ea6dd681954c9aa9c0934
rakshitnayak <rdsonnn@gmail.com>
2024-02-07 23:53:04 +0530

    Daily code - 1

---
+++ Code/daily/countChar.js
@@ -0,0 +1,25 @@
+function isAlphaNumeric(char) {
+  var code = char.charCodeAt(0);
+  if (
+    !(code > 47 && code < 58) && // numeric (0-9)
+    !(code > 64 && code < 91) && // upper alpha(A-Z)
+    !(code > 96 && code < 123) // lower alpha(a-z)
+  ) {
+    return false;
+  }
+
+  return true;
+}
+
+function countChar(str) {
+  var obj = {};
+  for (var char of str) {
+    if (isAlphaNumeric(char)) {
+      char = char.toLowerCase();
+      obj[char] = ++obj[char] || 1;
+    }
+  }
+  return obj;
+}
+
+console.log(countChar("Hi this is rakshit"));
