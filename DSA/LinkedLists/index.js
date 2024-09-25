3aac8420fcf458423234041ca318f73cb83e3bf3
rakshitnayak <rdsonnn@gmail.com>
2024-09-25 09:34:33 +0530

    Linked List search, reverse etc

--- DSA/LinkedLists/index.js
+++ DSA/LinkedLists/index.js
@@ -53,7 +53,7 @@ LinkedList.prototype.deleteFirstNode = function () {
   this.head = head.next;
 };
 
-//Delete the last node
+// Delete the last node
 LinkedList.prototype.deleteLastNode = function () {
   if (!this.head) {
     return;
@@ -70,3 +70,82 @@ LinkedList.prototype.deleteLastNode = function () {
 
   secondLast.next = null;
 };
+
+// Delete a node with given key
+LinkedList.prototype.deleteByKey = function (key) {
+  if (!this.head) {
+    console.log("list is empty");
+    return;
+  }
+
+  if (this.head.data === key) {
+    this.head = this.head.next;
+    return;
+  }
+
+  let current = this.head;
+
+  while (current.next !== null) {
+    if (current.next.data === key) {
+      current.next = current.next.next;
+      return;
+    }
+
+    current = current.next;
+  }
+};
+
+// Search operation
+LinkedList.prototype.search = function (key) {
+  let current = this.head;
+  while (current) {
+    if (current.next.data === key) {
+      return true;
+    }
+
+    current = current.next;
+  }
+
+  return false;
+};
+
+// Traversal operation
+LinkedList.prototype.printList = function () {
+  let current = this.head;
+
+  let listValues = [];
+  while (current) {
+    listValues.push(current.data);
+    current = current.next;
+  }
+
+  console.log(listValues.join("->"));
+};
+
+// reverse a linked list
+LinkedList.prototype.reverse = function () {
+  let current = this.head;
+  let next = null;
+  let prev = null;
+
+  while (current) {
+    next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+
+  this.head = prev;
+};
+
+/*run the code as below */
+const ll = new LinkedList();
+
+ll.insertAtBeginning(2);
+ll.insertAtEnd(10);
+
+ll.printList();
+
+ll.reverse();
+
+ll.printList();
