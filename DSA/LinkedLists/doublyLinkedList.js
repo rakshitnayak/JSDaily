be13ea731354f2eb2c33bd7a8000742580c76f7a
rakshitnayak <rdsonnn@gmail.com>
2024-11-02 19:59:10 +0530

    Doubly linked list at start & end

--- DSA/LinkedLists/doublyLinkedList.js
+++ DSA/LinkedLists/doublyLinkedList.js
@@ -12,3 +12,33 @@ class DoublyLinkedList {
     this.tail = null;
   }
 }
+
+// Insert at the beginning
+DoublyLinkedList.prototype.insertAtBeginning = function (data) {
+  const newNode = new Node(data, this.head, null);
+
+  if (this.head !== null) {
+    this.head.prev = newNode;
+  }
+
+  this.head = newNode;
+
+  if (this.tail === null) {
+    this.tail = newNode;
+  }
+};
+
+// Insert at the end
+DoublyLinkedList.prototype.insertAtEnd = function (data) {
+  const newNode = new Node(data, null, this.tail);
+
+  if (this.tail !== null) {
+    this.tail.next = newNode;
+  }
+
+  this.tail = newNode;
+
+  if (this.head === null) {
+    this.head = newNode;
+  }
+};
