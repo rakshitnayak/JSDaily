a7ee9426e2aceeb316a593aaf52ce3d8476f1441
rakshitnayak <rdsonnn@gmail.com>
2025-01-02 10:51:25 +0530

    rotate by k

---
+++ Code/daily/rotateByk.js
@@ -0,0 +1,16 @@
+var rotate = function (nums, k) {
+  k %= nums.length;
+  const reverse = (left, right) => {
+    while (left < right) {
+      [nums[left], nums[right]] = [nums[right], nums[left]];
+      left++;
+      right--;
+    }
+  };
+  reverse(0, nums.length - 1);
+  reverse(0, k - 1);
+  reverse(k, nums.length - 1);
+  return nums;
+};
+
+console.log(rotate([1, 2, 3, 4, 5, 6, 7], 3));
