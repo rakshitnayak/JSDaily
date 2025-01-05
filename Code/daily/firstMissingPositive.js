5816b7fd50a4217bf20938e4057e7d6c23724fa9
rakshitnayak <rdsonnn@gmail.com>
2025-01-06 00:49:23 +0530

    Daily leetcode 6th jan

---
+++ Code/daily/firstMissingPositive.js
@@ -0,0 +1,21 @@
+// this is leetcode hard problem
+function firstMissingPositive(nums) {
+  const n = nums.length;
+
+  for (let i = 0; i < n; i++) {
+    while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
+      let temp = nums[nums[i] - 1];
+      nums[nums[i] - 1] = nums[i];
+      nums[i] = temp;
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    if (nums[i] !== i + 1) {
+      return i + 1;
+    }
+  }
+
+  return n + 1;
+}
+console.log(firstMissingPositive([100000, 3, 4000, 2, 15, 1, 99999]));
---
+++ Code/daily/validSudoko.js
@@ -0,0 +1,39 @@
+var isValidSudoku = function (board) {
+  let seen = new Set();
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      let cellValue = board[i][j];
+      let boxIndex = Math.floor(i / 3) * 3 + Math.floor(j / 3);
+
+      if (cellValue !== ".") {
+        const rowVal = `row ${i} ${cellValue}`;
+        const colVal = `col ${j} ${cellValue}`;
+        const boxVal = `box ${boxIndex} - ${cellValue}`;
+        console.log(seen);
+        if (seen.has(rowVal) || seen.has(colVal) || seen.has(boxVal)) {
+          return false;
+        }
+
+        seen.add(rowVal);
+        seen.add(colVal);
+        seen.add(boxVal);
+      }
+    }
+  }
+
+  return true;
+};
+
+console.log(
+  isValidSudoku([
+    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+  ])
+);
