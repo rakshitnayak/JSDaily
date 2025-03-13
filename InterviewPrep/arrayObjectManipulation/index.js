98b30ea8b7596bab6f01e88aeafc88e7ff7b5e32
rakshitnayak <rdsonnn@gmail.com>
2025-03-13 20:45:56 +0530

    couple of more questions

--- InterviewPrep/arrayObjectManipulation/index.js
+++ InterviewPrep/arrayObjectManipulation/index.js
@@ -42,3 +42,62 @@ function findLatest(arr) {
 }
 
 console.log(findLatest(stateChanges));
+
+/** 
+ 
+Find Duplicate Values in an Array of Objects
+
+const items = [
+  { id: 1, name: "apple" },
+  { id: 2, name: "banana" },
+  { id: 3, name: "apple" },
+  { id: 4, name: "orange" },
+];
+
+Expected output:
+[
+  { id: 1, name: "apple" },
+  { id: 3, name: "apple" },
+] 
+
+*/
+const items = [
+  { id: 1, name: "apple" },
+  { id: 2, name: "banana" },
+  { id: 3, name: "apple" },
+  { id: 4, name: "orange" },
+];
+
+function findDuplicates(arr) {
+  const noOfItems = arr.reduce((acc, item) => {
+    acc[item.name] = (acc[item.name] || 0) + 1;
+    return acc;
+  }, {});
+
+  return arr.filter((item) => noOfItems[item.name] > 1);
+}
+
+console.log(findDuplicates(items));
+
+/**
+Flatten array
+const nestedArray = [1, [2, 3], [4, [5, 6]], 7];
+Expected output:
+[1, 2, 3, 4, 5, 6, 7]
+*/
+
+function flattenArray(arr) {
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      result = result.concat(flattenArray(arr[i]));
+    } else {
+      result.push(arr[i]);
+    }
+  }
+
+  return result;
+}
+
+console.log(flattenArray([1, [2, 3], [4, [5, 6]], 7]));
